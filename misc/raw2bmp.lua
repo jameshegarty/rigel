e8414d8bb95615221a7827d8ddf92522d3ce310a
James Hegarty <jameshegarty@gmail.com>
2017-02-28 00:36:50 -0800

    cleanup

--- misc/raw2bmp.lua
+++ misc/raw2bmp.lua
@@ -1,8 +1,5 @@
 darkroom={}
 require("common")
---Image = require("image")
---cstdlib = terralib.includec("stdlib.h")
---cstdio = terralib.includec("stdio.h")
 ffi = require "ffi"
 
 local metadata = dofile(arg[3])
@@ -36,7 +33,6 @@ function writeBmp(filename,width,height,stride,channels,data)
     return 0;
   end
 
-  --unsigned int totalsize=width * height * 3 +54; // we always write 3 channels
   local totalsize=ffi.new("unsigned int[1]",width * height * 3 +54)
     
   if ((ffi.C.fwrite(totalsize, 4, 1, file)) ~= 1) then
@@ -69,9 +65,7 @@ function writeBmp(filename,width,height,stride,channels,data)
     return 0;
   end
     
-  --unsigned int owidth=width;
   local owidth = ffi.new("unsigned int[1]",width)
-  --unsigned int oheight=height;
   local oheight = ffi.new("unsigned int[1]",height)
     
   --// write the width
@@ -88,7 +82,6 @@ function writeBmp(filename,width,height,stride,channels,data)
     
     
   --// write the planes
-  --unsigned short int planes=1;
   local planes = ffi.new("unsigned short[1]",1)
     
   if ((ffi.C.fwrite(planes, 2, 1, file)) ~= 1) then
@@ -97,7 +90,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --// write the bpp
-  --unsigned short bitsPP = 24;
   local bitsPP = ffi.new("unsigned short[1]",24)
     
   if ((ffi.C.fwrite(bitsPP, 2, 1, file)) ~= 1) then
@@ -106,7 +98,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --//compression
-  --unsigned int comp=0;
   local comp = ffi.new("unsigned int[1]",0)
     
   if ((ffi.C.fwrite(comp, 4, 1, file)) ~= 1) then
@@ -115,7 +106,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --//imagesize
-  --comp=0;
   comp[0]=0
     
   if ((ffi.C.fwrite(comp, 4, 1, file)) ~= 1) then
@@ -124,7 +114,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --//XpixelsPerM
-  --comp=0;
   comp[0]=0
     
   if ((ffi.C.fwrite(comp, 4, 1, file)) ~= 1) then
@@ -133,7 +122,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --//YpixelsPerM
-  --comp=0;
   comp[0]=0
     
   if ((ffi.C.fwrite(comp, 4, 1, file)) ~= 1) then
@@ -142,7 +130,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --//ColorsUsed
-  --comp=0;
   comp[0]=0
     
   if ((ffi.C.fwrite(comp, 4, 1, file)) ~= 1) then
@@ -151,7 +138,6 @@ function writeBmp(filename,width,height,stride,channels,data)
   end
     
   --//ColorsImportant
-  --comp=0;
   comp[0]=0
      
   if ((ffi.C.fwrite(comp, 4, 1, file)) ~= 1) then
@@ -165,29 +151,22 @@ function writeBmp(filename,width,height,stride,channels,data)
     print("[Bmp save] No image data to write!");
     return 0;	
   end
-    
-  --unsigned char temp;
 
   --// bmps pad each row to be a multiple of 4 bytes
-  --int padding = 4 - (width * 3 % 4); // we always write 3 channels
   local padding = 4 - (width * 3 % 4)
-  --padding = padding == 4 ? 0 : padding;
   if padding==4 then padding=0 end
-  --char pad[]={0,0,0};
   local pad = ffi.new("char[3]")
   pad[0]=0; pad[1]=0; pad[2]=0;
 
   if(channels==3)then
     -- calculate the size (assuming 24 bits or 3 bytes per pixel).
-    --unsigned long size = height * width * channels;
+
     local size = height * width * channels;
 
-    --unsigned char* tempData = malloc(sizeof(unsigned char)*width*height*3);
     local tempData = ffi.new("unsigned char["..tostring(width*height*3).."]")
 
-    --for (unsigned int y=0; y<height; y++) then -- reverse all of the colors. (bgr -> rgb)
+
     for y=0,height-1 do
-      --for (unsigned int x=0; x<width*3; x+=3)then
       for x=0,width*3-1,3 do
         local i=y*width*3+x;
         local ii = y*stride*3+x;
@@ -198,7 +177,6 @@ function writeBmp(filename,width,height,stride,channels,data)
     end
     
     if(padding ~= 0)then
-      --for(int h=0; h<height; h++)then
       for h=0,height-1 do
         ffi.C.fwrite(tempData[h*width*channels], width*channels, 1, file);
         ffi.C.fwrite(pad,padding,1,file);
@@ -209,17 +187,12 @@ function writeBmp(filename,width,height,stride,channels,data)
         return 0;
       end
     end
-
-    --free(tempData);
       
   elseif channels==1 then
     --//we need to expand an alpha into 3 components so taht it can actually be read
-    --unsigned char* expanded=malloc(sizeof(unsigned char)*width*height*3);
-      local expanded = ffi.new("unsigned char["..tostring(width*height*3).."]")
+    local expanded = ffi.new("unsigned char["..tostring(width*height*3).."]")
       
-    --for(int y=0; y<height; y++)then
     for y=0,height-1 do
-      --for(int x=0; x<width; x++)then
       for x=0,width-1 do
         local i = (y*width+x)*3;
         local ii = y*stride+x;
@@ -230,7 +203,6 @@ function writeBmp(filename,width,height,stride,channels,data)
     end
        
     if(padding ~= 0)then
-      --for(int h=0; h<height; h++)then
       for h=0,height-1 do
         ffi.C.fwrite(expanded[h*width*3], width*3, 1, file);
         ffi.C.fwrite(pad,padding,1,file);
@@ -238,264 +210,21 @@ function writeBmp(filename,width,height,stride,channels,data)
     else
       if ((ffi.C.fwrite(expanded,width*height*3, 1, file)) ~= 1) then
         print("[Bmp save] Error writing image data");
-        --//delete[] expanded;
-        --free(expanded);
         return 0;
       end
     end
 
-    --//delete[] expanded;
-    --free(expanded);
   else
     print("Error, image has %d channels!\n",channels);
-    --assert(0); 
     return 0
   end
     
     
   ffi.C.fclose(file);
 
---  // were done.
   return 1;
-
---  return file
 end
 
---[==[
-local chack = terralib.includecstring [=[
-#include<stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-
-int bsaveBMP_UC(void* ifile, int width, int height, int stride, int channels, void* idata){
-
-  FILE* file = ifile;
-  unsigned char* data = idata;
-//  FILE *file;
-    
-  // make sure the file is there.
-//  if ((file = fopen(filename, "wb"))==NULL){
-//    printf("could't open file for writing %s",filename);
-//    return 0;
-//  }
-    
-//  unsigned char BMPheader[2]={66,77};
-
-//  if ((fwrite(&BMPheader, sizeof(BMPheader), 1, file)) != 1) {
-//    printf("[Bmp::save] Error writing header");
-//    return 0;
-//  }
-    
-  unsigned int totalsize=width * height * 3 +54; // we always write 3 channels
-    
-//  if ((fwrite(&totalsize, sizeof(totalsize), 1, file)) != 1) {
-//    printf("[Bmp::save] Error writing header");
-//    return 0;
-//  }
-
-  totalsize=0;
-    
-  if ((fwrite(&totalsize, sizeof(totalsize), 1, file)) != 1) {
-    printf("[Bmp::save] Error writing header");
-      
-    return 0;
-      
-  }
-    
-  //offset to data=54
-  totalsize=54;
-    
-  if ((fwrite(&totalsize, sizeof(totalsize), 1, file)) != 1) {
-    printf("[Bmp::save] Error writing header");
-      
-    return 0;
-      
-  }
-    
-  //	Size of InfoHeader =40 
-  totalsize=40;
-    
-  if ((fwrite(&totalsize, sizeof(totalsize), 1, file)) != 1) {
-    printf("[Bmp::save] Error writeing header");
-      
-    return 0;
-      
-  }
-    
-  unsigned int owidth=width;
-  unsigned int oheight=height;
-    
-  // write the width
-  if ((fwrite(&owidth, 4, 1, file)) != 1) {
-    printf("Error writeing width");
-      
-    return 0;
-      
-  }
-    
-  // write the height 
-  if ((fwrite(&oheight, 4, 1, file)) != 1) {
-    printf("Error writing height");
-      
-    return 0;
-  }
-    
-    
-  // write the planes
-  unsigned short int planes=1;
-    
-  if ((fwrite(&planes, 2, 1, file)) != 1) {
-    printf("Error writeing planes");
-      
-    return 0;
-  }
-    
-  // write the bpp
-  unsigned short bitsPP = 24;
-    
-  if ((fwrite(&bitsPP, 2, 1, file)) != 1) {
-    printf("[Bmp save] Error writeing bpp");
-    return 0;
-  }
-    
-  //compression
-  unsigned int comp=0;
-    
-  if ((fwrite(&comp, 4, 1, file)) != 1) {
-    printf("Error writeing planes to ");
-    return 0;
-  }
-    
-  //imagesize
-  comp=0;
-    
-  if ((fwrite(&comp, 4, 1, file)) != 1) {
-    printf("Error writeing planes to ");
-    return 0;
-  }
-    
-  //XpixelsPerM
-  comp=0;
-    
-  if ((fwrite(&comp, 4, 1, file)) != 1) {
-    printf("Error writeing planes to ");
-    return 0;
-  }
-    
-  //YpixelsPerM
-  comp=0;
-    
-  if ((fwrite(&comp, 4, 1, file)) != 1) {
-    printf("Error writeing planes to ");
-    return 0;
-  }
-    
-  //ColorsUsed
-  comp=0;
-    
-  if ((fwrite(&comp, 4, 1, file)) != 1) {
-    printf("Error writeing planes to ");
-    return 0;
-  }
-    
-  //ColorsImportant
-  comp=0;
-     
-  if ((fwrite(&comp, 4, 1, file)) != 1) {
-    printf("Error writeing planes to ");
-    return 0;
-  }
-    
-  // read the data. 
-    
-  if (data == NULL) {
-    printf("[Bmp save] No image data to write!");
-    return 0;	
-  }
-    
-  unsigned char temp;
-
-  // bmps pad each row to be a multiple of 4 bytes
-  int padding = 4 - (width * 3 % 4); // we always write 3 channels
-  padding = padding == 4 ? 0 : padding;
-  char pad[]={0,0,0};
-
-  if(channels==3){
-    // calculate the size (assuming 24 bits or 3 bytes per pixel).
-    unsigned long size = height * width * channels;
-
-    unsigned char* tempData = malloc(sizeof(unsigned char)*width*height*3);
-
-    for (unsigned int y=0; y<height; y++) { // reverse all of the colors. (bgr -> rgb)
-      for (unsigned int x=0; x<width*3; x+=3){
-        int i=y*width*3+x;
-        int ii = y*stride*3+x;
-        tempData[i] = data[ii+2];
-        tempData[i+1] = data[ii+1];
-        tempData[i+2] = data[ii];
-      }
-    }
-    
-    if(padding != 0){
-      for(int h=0; h<height; h++){
-        fwrite(&tempData[h*width*channels], width*channels, 1, file);
-        fwrite(pad,padding,1,file);
-      }
-    }else{
-      if ((fwrite(tempData, size, 1, file)) != 1) {
-        printf("[Bmp save] Error writeing image data");
-        return 0;
-      }
-    }
-
-    free(tempData);
-      
-  }else if(channels==1){
-    //we need to expand an alpha into 3 components so taht it can actually be read
-    //unsigned char* expanded=new unsigned char[width*height*3];
-    unsigned char* expanded=malloc(sizeof(unsigned char)*width*height*3);
-      
-    for(int y=0; y<height; y++){
-      for(int x=0; x<width; x++){
-        int i = (y*width+x)*3;
-        int ii = y*stride+x;
-        expanded[i]=data[ii];
-        expanded[i+1]=data[ii];
-        expanded[i+2]=data[ii];
-      }
-    }
-       
-    if(padding != 0){
-      for(int h=0; h<height; h++){
-        fwrite(&expanded[h*width*3], width*3, 1, file);
-        fwrite(pad,padding,1,file);
-      }
-    }else{
-      if ((fwrite(expanded,width*height*3, 1, file)) != 1) {
-        printf("[Bmp save] Error writing image data");
-        //delete[] expanded;
-        free(expanded);
-        return 0;
-      }
-    }
-
-    //delete[] expanded;
-    free(expanded);
-  }else{
-    printf("Error, image has %d channels!\n",channels);
-    assert(0); 
-  }
-    
-    
-  fclose(file);
-
-  // were done.
-  return 1;
-}
-]=]
-
-]==]
 
 function raw2bmp(infile, outfile, axiround)
   local totalSize = metadata.outputWidth*metadata.outputHeight*metadata.outputBytesPerPixel
@@ -527,7 +256,6 @@ function raw2bmp(infile, outfile, axiround)
     os.exit(1)
   end
 
-
   ffi.C.fclose(imgIn)
 
   -- we can't write 3 channels per pixel over axi.
@@ -541,9 +269,6 @@ function raw2bmp(infile, outfile, axiround)
 
   local file = writeBmp(outfile,metadata.outputWidth, metadata.outputHeight, metadata.outputWidth,
                      metadata.outputBytesPerPixel, dataPtr)
-
---  chack.bsaveBMP_UC(file, metadata.outputWidth, metadata.outputHeight, metadata.outputWidth,
---                    metadata.outputBytesPerPixel, dataPtr)
 end
 
 raw2bmp(arg[1], arg[2], arg[4]=="1")
