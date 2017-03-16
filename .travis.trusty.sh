c5d1bdbe8458d83bd8ecbbe970b72c695bb8a332
James Hegarty <jameshegarty@gmail.com>
2017-03-15 17:00:09 -0700

    trusty script

--- .travis.trusty.sh
+++ .travis.trusty.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install luajit
-sudo apt-get install verilator
+apt-get install luajit
+apt-get install verilator
 wget https://github.com/zdevito/terra/releases/download/release-2016-02-26/terra-Linux-x86_64-2fa8d0a.zip
 unzip terra-Linux-x86_64-2fa8d0a.zip
-sudo ln -s /home/travis/build/jameshegarty/rigel/terra-Linux-x86_64-2fa8d0a/bin/terra /usr/bin/terra
+ln -s /home/travis/build/jameshegarty/rigel/terra-Linux-x86_64-2fa8d0a/bin/terra /usr/bin/terra
