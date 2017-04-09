55ca582256018eac7b8b6b7a72b59048139d56f6
James Hegarty <jameshegarty@gmail.com>
2017-04-08 21:57:32 -0700

    disabled failing test

--- misc/crontop.sh
+++ misc/crontop.sh
@@ -7,7 +7,12 @@ DATE=`date +%Y-%m-%d`
 if /home/jhegarty/rigel/misc/cron.sh > $DATE.txt 2>&1; then
   echo "OK"
   mv $DATE.txt $DATE.ok.txt
+  git add $DATE.ok.txt
 else
   echo "FAIL"
   mv $DATE.txt $DATE.fail.txt
+  git add $DATE.fail.txt
 fi
+
+git commit -m "build status"
+git push
