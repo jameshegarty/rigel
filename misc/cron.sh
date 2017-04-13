e0959975b688139dd16434954cf656a0c036998f
James Hegarty <jameshegarty@gmail.com>
2017-04-12 23:25:34 -0700

    zynq100 target fix

--- misc/cron.sh
+++ misc/cron.sh
@@ -15,5 +15,7 @@ cd /home/jhegarty/rigelcron
 rm -Rf /home/jhegarty/rigelcron/rigel
 git clone https://github.com/jameshegarty/rigel.git
 cd rigel/examples
-make -j64 axibits
+make -j64 axibits zynq100bits
+make zynq100
 make axi
+
