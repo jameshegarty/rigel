ae36048d0408afc3f6e4f320e1fba1f66e3206a9
James Hegarty <jameshegarty@gmail.com>
2017-05-03 15:30:03 -0700

    synopsys cron script

--- misc/cron.sh
+++ misc/cron.sh
@@ -11,6 +11,21 @@ export LD_LIBRARY_PATH=
 export PATH=/opt/Xilinx/14.5/ISE_DS/ISE/bin/lin64:/opt/Xilinx/14.5/ISE_DS/ISE/sysgen/util:/opt/Xilinx/14.5/ISE_DS/ISE/sysgen/bin:/opt/Xilinx/14.5/ISE_DS/ISE/../../../DocNav:/opt/Xilinx/14.5/ISE_DS/PlanAhead/bin:/opt/Xilinx/14.5/ISE_DS/EDK/bin/lin64:/opt/Xilinx/14.5/ISE_DS/EDK/gnu/microblaze/lin64/bin:/opt/Xilinx/14.5/ISE_DS/EDK/gnu/powerpc-eabi/lin64/bin:/opt/Xilinx/14.5/ISE_DS/EDK/gnu/arm/lin/bin:/opt/Xilinx/14.5/ISE_DS/EDK/gnu/microblaze/linux_toolchain/lin64_be/bin:/opt/Xilinx/14.5/ISE_DS/EDK/gnu/microblaze/linux_toolchain/lin64_le/bin:/opt/Xilinx/14.5/ISE_DS/common/bin/lin64:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/jhegarty/terra-Linux-x86_64-84bbb0b/bin
 echo $PATH
 
+##################
+export MODULESHOME=/afs/ir.stanford.edu/class/ee/modules/tcl
+source $MODULESHOME/init/bash.in
+
+module load base
+#module load vcs
+module load syn
+module load icc
+
+export ARCH_OVERRIDE=linux
+
+# load in my symbolic libraries to prevent synopsys from complaining
+export LD_LIBRARY_PATH="/home/jhegarty/synlib/:$LD_LIBRARY_PATH"
+##################
+
 cd /home/jhegarty/rigelcron
 rm -Rf /home/jhegarty/rigelcron/rigel
 git clone https://github.com/jameshegarty/rigel.git
