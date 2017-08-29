c457e1c1157b14bc5937c5a12ba8144dca2b3207
James Hegarty <jameshegarty@gmail.com>
2017-08-29 14:31:02 -0700

    ice40 test

---
+++ platform/ice/uarttest.py
@@ -0,0 +1,23 @@
+import serial
+import sys
+
+print "usage: test.py inputfile.raw outputfile.raw"
+
+print "infile:"+sys.argv[1]
+print "outfile:"+sys.argv[2]
+
+infile = open(sys.argv[1],"rb")
+outfile = open(sys.argv[2],"wb")
+
+indata = infile.read()
+print("infile len: ",len(indata))
+
+ser = serial.Serial("/dev/tty.usbserial-141B",115200)
+ser.write(indata)
+res = ser.read(len(indata))
+#print(x)
+outfile.write(res)
+ser.close()
+
+infile.close()
+outfile.close()
